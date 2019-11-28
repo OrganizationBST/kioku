a26e2302d1df4525318ba36c1327879d77b96a50
jgpat <jgpat@asu.edu>
2019-11-28 00:13:31 -0700

    created UI

---
+++ src/components/DeckList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styles from '../App.css';
+import uuid from 'uuid';
+
+class DeckList extends Component {
+    state = {
+        decks: [
+            { id: uuid(), name: 'Hiragana' },
+            { id: uuid(), name: 'Katakana' },
+        ]
+    }
+
+    render() {
+        const { decks } = this.state;
+        return(
+            <Container>
+                <Button
+                    style={{backgroundColor: '#db9989', marginBottom: '2rem', border: 'none'}}
+                    onClick={() => {
+                        const name = prompt('Name for deck:');
+                        if(name) {
+                            this.setState(state => ({
+                                decks: [...state.decks, { id: uuid(), name }] 
+                            }));
+                        }
+                    }}
+                >Create Deck</Button>
+
+                <ListGroup>
+                    <TransitionGroup className="deck-list">
+                        {decks.map(({ id, name}) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem className="item">
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        );
+    }
+}
+
+export default DeckList;
